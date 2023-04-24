ff241533c0b7796a387076281640eb8ef6341a89
Rex Remigius Stephen Jothi <rex.remigius@multicorewareinc.com>
2023-04-24 16:39:14 +0530

    Modified AddressSanitizer.cpp file
        Added code to make reallocation of array bound to make the legal
        access of memory.

--- lib/AddressSanitizer.cpp
+++ lib/AddressSanitizer.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 using namespace std;
 using namespace llvm;
-
+/*
 bool isUsedByInstruction(Instruction *inst1, Instruction *inst2) {
   for (Use &use : inst1->operands()) {
     if (use.get() == inst2) {
@@ -15,84 +15,102 @@ bool isUsedByInstruction(Instruction *inst1, Instruction *inst2) {
   }
   return false;
 }
-
-PreservedAnalyses AddressSanPass::run(Function &F,FunctionAnalysisManager &FAM) 
-{
-  string funcName = F.getName().str();
-  vector<Instruction *> instructions;
-  if (!(funcName.find("asan") != string::npos)) 
-  {
-  for(auto &BB:F)
-    {
-        for(auto &inst:BB)
-        {
-            errs()<<inst<<"\n";
-            if(isa<CallInst>(inst))
-            {
-               // errs()<<"call instruction\n";
-                auto *callInst=dyn_cast<CallInst>(&inst);
-                string call_inst=callInst->getCalledFunction()->getName().str();
-                if(call_inst.find("__asan_report_")!=string::npos)
-                {
-                   // errs()<<"asan call ......\n";
-                    // auto *illegal=dyn_cast<Instruction>(callInst->getOperand(0));Stack dump:
-                    // auto *memory_access=dyn_cast<Instruction>(illegal->getOperand(0));
-                    auto *next = callInst->getNextNode();
-                    instructions.push_back(next);
-                    //to access previous block
-                    auto *prevBB = BB.getPrevNode();
-                    if (prevBB != nullptr) {
-                    errs() << "Previous BB : " << *prevBB << "\n";
-                    //to get last instruction of previous block
-                    auto *lastInst = prevBB->getTerminator();
-                    errs() << *lastInst << "\n";
-                      if (lastInst) {
-                        BasicBlock *target = lastInst->getSuccessor(1);
-                        IRBuilder<> builder(next);
-                        Instruction *BrInst = builder.CreateBr(target);
-                      errs() << *BrInst << "\n";
-                        lastInst->replaceAllUsesWith(BrInst);
-                      }
-                    } 
-                    //to get basic block where target instruction is present
-                    /*
-                    //first instruction of of next basic block
-                    Instruction* initial = nextBB->getFirstNonPHI();
-
-                    //target instruction where spliting take place
-                    Instruction* instToSplit=initial->getNextNode();
-
-                    //in case intial instruction is load instruction , check from second instruction to end of basic block whether instruction is depend on initial instruction
-                    if(initial->getOpcode()==Instruction::Load)
-                    {
-                    for (BasicBlock::iterator i = ++nextBB->begin(), i2 = nextBB->end(); i != i2; i++) 
-                    {
-                     auto *in = dyn_cast<Instruction>(i);
-                    
-                        if(isUsedByInstruction(in,initial))
-                        {
-                            instToSplit=in->getNextNode();
-                            continue;
-                        }
-                        break;
-                    }
-                    }
-
-                    //target basic block
-                    BasicBlock* newBlock = nextBB->splitBasicBlock(instToSplit);
-                    errs()<<*newBlock->getSinglePredecessor()<<" "<<*newBlock<<"\n";
 */
-                    
-                    
-                }
+PreservedAnalyses AddressSanPass::run(Function &F,
+                                      FunctionAnalysisManager &FAM) {
+  string funcName = F.getName().str();
+  LLVMContext &Ctx = F.getContext();
+  int initial_size;
 
-            }
+  vector<Instruction *> instructions;
+  if (!(funcName.find("asan") != string::npos)) {
+    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; bb++) {
+      for (BasicBlock::iterator i = bb->begin(), i2 = bb->end(); i != i2; i++) {
+        auto *inst = dyn_cast<Instruction>(i);
+        errs() << *inst << "\n";
+        Value *arr;
+        if (isa<AllocaInst>(inst)) {
+          if (inst->getNameOrAsOperand().find("retval") == string::npos) {
+            arr = inst;
+            errs() << " alloc : " << *arr << "\n";
+          }
         }
+        if (isa<CallInst>(inst)) {
+          // errs()<<"call instruction\n";
+          auto *callInst = dyn_cast<CallInst>(inst);
+          string call_inst = callInst->getCalledFunction()->getName().str();
+          if (call_inst.find("calloc") != string::npos) {
+            errs() << " Calloc : " << *callInst << "\n";
+            errs()<<*(callInst->getArgOperand(0))<<"\n";
+          }
+          if (call_inst.find("__asan_report_") != string::npos) {
+
+            // auto
+            // *illegal=dyn_cast<Instruction>(callInst->getOperand(0));Stack
+            // dump: auto
+            // *memory_access=dyn_cast<Instruction>(illegal->getOperand(0));
+            
+            auto *next = callInst->getNextNode();
+            
+            // Memory reallocation instrumentation code.
+            IRBuilder<> builder(next);
+            Type *fromTy = Type::getInt32PtrTy(Ctx);
+            Value *loadInst = builder.CreateLoad(fromTy,arr);
+            Type *toTy = Type::getInt8PtrTy(Ctx);
+            auto *bitinst32To8 = builder.CreateBitCast(loadInst,toTy);
+            auto *bitinst8To32 = builder.CreateBitCast(bitinst32To8,fromTy);
+            auto *storeInst = builder.CreateStore(bitinst8To32,arr);
+
+            instructions.push_back(next);
+
+            // to access previous block
+            auto *prevBB = bb->getPrevNode();
+            if (prevBB != nullptr) {
+              auto *lastInst = prevBB->getTerminator();
+              if (lastInst) {
+                BasicBlock *target = lastInst->getSuccessor(1);
+                IRBuilder<> builder(next);
+                Instruction *BrInst = builder.CreateBr(target);
+                lastInst->replaceAllUsesWith(BrInst);
+              }
+            }
+            // to get basic block where target instruction is present
+            /*
+            //first instruction of of next basic block
+            Instruction* initial = nextBB->getFirstNonPHI();
+
+            //target instruction where spliting take place
+            Instruction* instToSplit=initial->getNextNode();
+
+            //in case intial instruction is load instruction , check from second
+            instruction to end of basic block whether instruction is depend on
+            initial instruction if(initial->getOpcode()==Instruction::Load)
+            {
+            for (BasicBlock::iterator i = ++nextBB->begin(), i2 = nextBB->end();
+            i != i2; i++)
+            {
+             auto *in = dyn_cast<Instruction>(i);
+
+                if(isUsedByInstruction(in,initial))
+                {
+                    instToSplit=in->getNextNode();
+                    continue;
+                }
+                break;
+            }
+            }
+
+            //target basic block
+            BasicBlock* newBlock = nextBB->splitBasicBlock(instToSplit);
+            errs()<<*newBlock->getSinglePredecessor()<<" "<<*newBlock<<"\n";
+*/
+          }
+        }
+      }
     }
   }
-  for(auto *i : instructions){
+  for (auto *i : instructions) {
     i->eraseFromParent();
   }
-    return PreservedAnalyses::all();
-    
+  return PreservedAnalyses::all();
 }
